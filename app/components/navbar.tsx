b1cbc6cc316e494be9d52a5dae95d96340667244
vinayakfr <vinayak.sri@yahoo.com>
2024-06-24 02:19:02 +0530

    chore: modify navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <div>
-      <nav className="bg-black text-white p-4 flex justify-between items-center h-20 text-xl xl:text-2xl rounded-full mx-2 xl:mx-5">
+      <nav className="bg-black text-white p-4 flex justify-between items-center h-16 text-xl xl:text-2xl rounded-full my-4 mx-2 xl:mx-5">
         <a href="#" className="font-bold text-2xl xl:text-3xl ml-5 hover:underline cursor-pointer">
           HelpingHand
         </a>
