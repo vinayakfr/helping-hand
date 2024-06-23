820beab51fc268e3a28c40e029867021c024c8a8
vinayakfr <vinayak.sri@yahoo.com>
2024-06-23 15:15:54 +0530

    feat: add new section in volunteer registration

--- app/volunteer-registration/page.tsx
+++ app/volunteer-registration/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Navbar from "../components/navbar";
 
 function VolunteerReg() {
   return (
     <div>
-      <div className="flex place-content-center place-items-start px-10">
+      <Navbar/>
+      {/* <div className="flex place-content-center place-items-start px-10">
         <h1 className="xl:text-[8rem] font-bold">Volunteers Assemble</h1>
         <div className="flex flex-col gap-8 p-[3rem] bg-blck">
           <p className="xl:text-2xl font-light">
@@ -72,6 +74,33 @@ function VolunteerReg() {
           </div>
         </div>
         <button className="h-14 w-28 xl:h-16 xl:w-40 rounded-xl bg-black text-white font-thin hover:font-medium hover:bg-white hover:text-black hover:border-2 hover:border-black shadow-2xl hover:shadow-md hover:shadow-black">
+          <a href="" className="text-center md:text-lg xl:text-2xl">
+            Continue
+          </a>
+        </button>
+      </div> */}
+      <div className="flex flex-col gap-3 place-content-center place-items-center py-10">
+        <h1 className="font-bold xl:text-8xl">Tell us your story</h1>
+        <p className="text-center font-light xl:text-3xl px-10">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quos
+          placeat dicta adipisci delectus libero ullam incidunt saepe, sit
+          perspiciatis dolorum repellendus veritatis eveniet, neque illum
+          tenetur quaerat aliquid dolorem.
+        </p>
+        <div className="flex w-full justify-between place-content-center place-items-center px-24">
+          <div className="flex flex-col gap-2 place-content-center place-items-center xl:text-3xl font-thin italic ">
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing.</li>
+          </div>
+          <textarea
+            name="story"
+            id="story"
+            className="h-[25rem] w-[40rem] rounded-xl border-4 border-black p-3 xl:text-xl"
+            placeholder="I started volunteering because..."
+          />
+        </div>
+        <button className="h-14 w-28 xl:h-16 xl:w-40 rounded-xl bg-black text-white font-thin hover:font-medium hover:bg-white hover:text-black hover:border-2 hover:border-black shadow-2xl hover:shadow-md hover:shadow-black my-4">
           <a href="" className="text-center md:text-lg xl:text-2xl">
             Continue
           </a>
