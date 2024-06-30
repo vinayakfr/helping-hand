29a9dd9ddd0068643fa88a71b8cab0e39749d7c5
vinayakfr <vinayak.sri@yahoo.com>
2024-06-30 16:55:44 +0530

    chore: modify hero page

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       <p className="text-center text-lg lg:text-xl xl:text-2xl text-gray-600 px-4 mt-4">
         Don&apos;t worry, <span className="font-semibold">we&apos;ve got you covered.</span> We&apos;ll help you find the <span className="underline">perfect opportunity</span> that suits your skills and interests.
       </p>
-        <div className="flex flex-col gap-3 lg:gap-4 xl:gap-5 place-content-center place-items-center py-5 lg:py-3 xl:py-8">
+        <div className="flex flex-col gap-3 lg:gap-4 xl:gap-5 place-content-center place-items-center py-5 lg:py-3 xl:pt-8">
           <button className="h-14 w-28 xl:h-16 xl:w-40 rounded-xl bg-black text-white font-thin hover:font-medium hover:bg-white hover:text-black hover:border-2 hover:border-black shadow-2xl hover:shadow-md hover:shadow-black duration-300">
             <a href="" className="text-center md:text-lg xl:text-2xl">
               About Us
@@ -34,6 +34,7 @@ export default function Home() {
             Log in
           </a>
         </p>
+        <p className="text-center xl:text-3xl xl:py-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque pariatur hic repellendus perspiciatis. Voluptates eaque at consequuntur ducimus ipsum assumenda.</p>
         <img
           src="asset2.png"
           alt=""
