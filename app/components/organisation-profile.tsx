24770ebc60bee9d2fe5887578a10aa05319d7a38
vinayakfr <vinayak.sri@yahoo.com>
2024-07-05 01:23:28 +0530

    chore: modify organisation-profile component

--- app/components/organisation-profile.tsx
+++ app/components/organisation-profile.tsx
@@ -16,8 +16,18 @@ function OrgProfile() {
         />
       </div>
       <div className="flex lg:flex-col lg:gap-4 lg:pt-[22.5rem] relative px-10">
-        <h1 className="lg:text-6xl font-bold text-black">Organisation Name</h1>
-        <p className="bg-zinc-200 p-4 lg:text-2xl rounded-xl">
+        <div className="flex justify-between w-full">
+          <h1 className="lg:text-6xl font-bold text-black">
+            Organisation Name
+          </h1>
+          <div className="flex place-content-center place-items-center gap-4">
+            <p className="font-semibold lg:text-xl">Currently hiring</p>
+            <button className="bg-black text-white hover:text-black hover:bg-white border-2 hover:border-4 hover:border-black font-medium hover:font-bold lg:w-24 lg:h-14 rounded-r-full rounded-l-full hover:shadow-xl">
+              <span className="text-xl">Apply</span>
+            </button>
+          </div>
+        </div>
+        <p className="bg-zinc-200 p-3 rounded-xl lg:text-2xl">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
           voluptatem neque necessitatibus praesentium, placeat velit quasi
           culpa, voluptate voluptatibus consequatur amet quibusdam, ad vero
@@ -49,7 +59,15 @@ function OrgProfile() {
               <h1 className="font-vold lg:text-4xl underline text-white">
                 Mission
               </h1>
-              <p className="font-light text-white text-center p-10 lg:text-lg">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime consequatur modi omnis labore, id odit ad aut maiores nostrum suscipit! Perspiciatis, reiciendis dignissimos nesciunt repudiandae mollitia aliquid eius voluptates aperiam necessitatibus exercitationem ullam ducimus! Adipisci repellat beatae voluptate facilis aliquam id aperiam, ex possimus explicabo culpa, enim assumenda quam officiis!</p>
+              <p className="font-light text-white text-center p-10 lg:text-lg">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime
+                consequatur modi omnis labore, id odit ad aut maiores nostrum
+                suscipit! Perspiciatis, reiciendis dignissimos nesciunt
+                repudiandae mollitia aliquid eius voluptates aperiam
+                necessitatibus exercitationem ullam ducimus! Adipisci repellat
+                beatae voluptate facilis aliquam id aperiam, ex possimus
+                explicabo culpa, enim assumenda quam officiis!
+              </p>
             </div>
             <img
               src=""
